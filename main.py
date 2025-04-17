29bbd5e15d873a881bf9b908ff907ecc395a3653
raidvoltus <nurcholismuhammad@gmail.com>
2025-04-17 23:50:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -316,7 +316,7 @@ if __name__ == "__main__":
             f"<b>💩Hai KONTIL Clan Member💩</b>\n"
             f"<b>Apapun Yang Sedang Kalian Hadapi Saat Ini, Ingatlah...</b>\n"
             f"<b><i>{motivation}</i></b>\n\n"
-            f"Berikut Top 5 saham pilihan berdasarkan analisa NUNG AI:\n"
+            f"<b>Berikut Top 5 saham pilihan berdasarkan analisa NUNG AI:</b>\n"
         )
         for r in top_5:
             message += (
